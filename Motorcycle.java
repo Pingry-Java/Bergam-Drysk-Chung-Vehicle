196546ae8f717947f7ccec971decfb27fa014f31
Miro Bergam <mbergam2019@pingry.org>
2017-02-23 21:43:30 -0500

    Overcode static variables + write constructors for all subclasses

--- Motorcycle.java
+++ Motorcycle.java
@@ -1,4 +1,24 @@
 public class Motorcycle extends Vehicle
 {
-	private static double maxGas = 10;
+	//Overriding static fields
+	private static double fuelCapacity = 40;
+	private static int seats = 2;
+	private static int wheels = 2; 
+	private static double cargoCapacity = 100;
+	private static double baseWeight = 500;
+ 
+ 
+	public Motorcycle()
+	{
+		time = 0;
+		forwardProgress = 0;
+		money = 1000; 
+		passengers = 2;  
+		cargo = 0;
+		fuel = fuelCapacity; 
+		tires = 1; //1 spare 
+		speed = 65;
+		engine = new Engine(4); 
+	}
+ 
 }
